16088823d477c0b018dabc14a74f5399f9cfc590
Sardo <sardo.ip@sardo.work>
2017-08-17 19:22:15 +0800

    Add default background image

--- src/main.js
+++ src/main.js
@@ -4,24 +4,22 @@ import editor from './components/editor.vue';
 
 Vue.use(VueHtml5Editor, {
   visibleModules: [
-      "text",
-      "color",
-      "font",
-      "align",
-      "list",
-      "link",
-      "unlink",
-      "tabulation",
-      "image",
-      "hr",
-      "eraser",
-      "undo",
-      "full-screen",
-      "info",
+      'text',
+      'font',
+      'align',
+      'list',
+      'link',
+      'unlink',
+      'tabulation',
+      'image',
+      'hr',
+      'eraser',
+      'undo',
+      'info',
   ],
 });
 new Vue({
-  el: '#app',
+  el: '#sparkle-editor',
    components: {
       editor
    }
