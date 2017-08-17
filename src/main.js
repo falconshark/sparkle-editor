838f5874eae4fb1687331103599843bab49885cc
Sardo <sardo.ip@sardo.work>
2017-08-17 21:53:49 +0800

    Hiddenable toolbar

--- src/main.js
+++ src/main.js
@@ -24,3 +24,17 @@ new Vue({
       editor
    }
 })
+
+$('.toolbar-trigger').hover(showToolbar);
+$('.content').on('click', hiddenToolbar);
+
+function showToolbar(){
+    $('.toolbar').removeClass('animated slideOutUp');
+    $('.toolbar').addClass('animated slideInDown');
+	$('.toolbar').css('opacity', '1');
+}
+
+function hiddenToolbar(){
+    $('.toolbar').removeClass('animated slideInDown');
+    $('.toolbar').addClass('animated slideOutUp');
+}
